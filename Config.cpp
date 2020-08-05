ef0103242e460561d545e6003ee62e3926bebb63
RomboCat <69248287+RomboCat@users.noreply.github.com>
2020-08-05 18:10:25 +0300

    Update Config.cpp

--- Config.cpp
+++ Config.cpp
@@ -8,7 +8,8 @@ class CfgPatches {
 		weapons[] = {};
 	};
 };
-class CfgMagazines;
+class CfgMagazines
+{
 class 1Rnd_HE_Grenade_shell;
 class rhs_VOG25: 1Rnd_HE_Grenade_shell;
 class rhs_VG40OP_white: rhs_VOG25;
@@ -21,4 +22,5 @@ class rhs_VG40OP_red: rhs_VG40OP_white {
 	displayName	= "ВГ-400П";
 	displayNameShort = "Красный";
 	descriptionShort = Тип: Осветительная граната<br/>Взрыватель: Запал<br/>Используется: ГП-25
+};
 };
