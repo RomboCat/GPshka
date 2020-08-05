78a248d300fa490e7cf093ae4480154ee6f895d0
RomboCat <69248287+RomboCat@users.noreply.github.com>
2020-08-05 16:17:56 +0300

    Update Config.cpp

--- Config.cpp
+++ Config.cpp
@@ -8,3 +8,9 @@ class CfgPatches {
 		weapons[] = {};
 	};
 };
+class rhs_VG40OP_green: rhs_VG40OP_white {
+	MAG_DESC_GP25_COLORED(ВГ-400П,зеленый,осветительная граната,запал (3 секунды));
+};
+class rhs_VG40OP_red: rhs_VG40OP_white {
+	MAG_DESC_GP25_COLORED(ВГ-400П,красный,осветительная граната,запал (3 секунды));
+};
