76cc09dfded7e6813fb5ade1eeb15a67f8d4c5c7
RomboCat <69248287+RomboCat@users.noreply.github.com>
2020-08-05 19:05:15 +0300

    Update Config.cpp

--- Config.cpp
+++ Config.cpp
@@ -8,19 +8,39 @@ class CfgPatches {
 		weapons[] = {};
 	};
 };
+class CfgAmmo
+{
+	class F_40mm_white;
+	class rhs_VG40OP_purple: F_40mm_white{};
+}
 class CfgMagazines
 {
 class 1Rnd_HE_Grenade_shell;
 class rhs_VOG25: 1Rnd_HE_Grenade_shell{};
 class rhs_VG40OP_white: rhs_VOG25{};
 class rhs_VG40OP_green: rhs_VG40OP_white {
-	displayName	= "ВГ-400П";
-	displayNameShort = "Зеленый";
-	descriptionShort = Тип: Осветительная граната<br/>Взрыватель: Запал<br/>Используется: ГП-25
+	displayName	= "ВГ-400П (зеленый)";
+	displayNameShort = "ВГ-400П";
+	descriptionShort = Тип: Осветительная граната<br/>Взрыватель: Запал (3 секунды)<br/>Используется: ГП-25
 };
 class rhs_VG40OP_red: rhs_VG40OP_white {
-	displayName	= "ВГ-400П";
-	displayNameShort = "Красный";
-	descriptionShort = Тип: Осветительная граната<br/>Взрыватель: Запал<br/>Используется: ГП-25
+	displayName	= "ВГ-400П (красный)";
+	displayNameShort = "ВГ-400П";
+	descriptionShort = Тип: Осветительная граната<br/>Взрыватель: Запал (3 секунды)<br/>Используется: ГП-25
+};
+class rhs_VG40OP_purple;
+class rhs_VG40OP_purpleCat: rhs_VG40OP_purple
+{
+	lightColor = [1, 0, 1, 1];
+	displayName	= "ВГ-400П (фиолетовый)";
+	displayNameShort = "ВГ-400П";
+	descriptionShort = Тип: Осветительная граната<br/>Взрыватель: Запал (3 секунды)<br/>Используется: ГП-25
+};
+class rhs_VG40OP_test: rhs_VG40OP_purple
+{
+	lightColor = [0.4, 0.3, 0.1, 0];
+	displayName	= "ВГ-400П (тест)";
+	displayNameShort = "ВГ-400П";
+	descriptionShort = Тип: Осветительная граната<br/>Взрыватель: Запал (3 секунды)<br/>Используется: ГП-25
 };
 };
