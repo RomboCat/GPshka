4e09a4bfc2e50c7c9a12efc9d9a27d93f2018ae6
RomboCat <69248287+RomboCat@users.noreply.github.com>
2020-08-05 16:32:26 +0300

    Update Config.cpp

--- Config.cpp
+++ Config.cpp
@@ -9,11 +9,9 @@ class CfgPatches {
 	};
 };
 class rhs_VG40OP_white
-{
 class rhs_VG40OP_green: rhs_VG40OP_white {
 	MAG_DESC_GP25_COLORED(ВГ-400П,зеленый,осветительная граната,запал (3 секунды));
 };
 class rhs_VG40OP_red: rhs_VG40OP_white {
 	MAG_DESC_GP25_COLORED(ВГ-400П,красный,осветительная граната,запал (3 секунды));
-};
 };
