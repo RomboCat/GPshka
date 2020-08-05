851c834efcd485cd97237376d10f82b3239d5991
RomboCat <69248287+RomboCat@users.noreply.github.com>
2020-08-05 16:04:13 +0300

    Update Config.cpp

--- Config.cpp
+++ Config.cpp
@@ -1,1 +1,10 @@
-
+class CfgPatches {
+	class gp_reconfig {
+		author = "Cat";
+		url = "nety(";
+		requiredVersion = 0.1;
+		requiredAddons[] = {"rbcmods_reconfig"};
+		units[] = {};
+		weapons[] = {};
+	};
+};
